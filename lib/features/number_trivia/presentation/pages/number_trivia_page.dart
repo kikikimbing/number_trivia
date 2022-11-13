a0b77dbd4de5bee58d3fbc9274364f00d53c24a7
Ananta <ananta.tyastama@mekari.com>
2022-11-13 15:33:12 +0700

    Create apps with flutter_bloc and clean architecture

---
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:number_trivia/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../injection_container.dart';
+import '../widgets/widget.dart';
+
+class NumberTriviaPage extends StatelessWidget {
+  const NumberTriviaPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Number Trivia"),
+      ),
+      body: SingleChildScrollView(child: buildBody(context)),
+    );
+  }
+
+  BlocProvider<NumberTriviaBloc> buildBody(BuildContext context) {
+    return BlocProvider(
+      create: (_) => sl<NumberTriviaBloc>(),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
+                builder: (context, state) {
+                  if (state is Empty) {
+                    return MessageDisplay(
+                      message: 'Start Searching!',
+                    );
+                  } else if (state is Loading) {
+                    return LoadingWidget();
+                  } else if (state is Loaded) {
+                    return TriviaDisplay(numberTrivia: state.trivia);
+                  } else if (state is Error) {
+                    return MessageDisplay(
+                      message: state.message,
+                    );
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+              TriviaControls(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
